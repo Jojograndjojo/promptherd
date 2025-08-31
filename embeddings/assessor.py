20da08f5fde2734b3f15b967f6dffbb36d26ef94
jpasse <jpasse@thoughtworks.com>
2025-08-31 11:23:54 +0200

    docs(EmbeddingsAssessor): add method descriptions

--- embeddings/assessor.py
+++ embeddings/assessor.py
@@ -4,10 +4,12 @@ import numpy as np
 class EmbeddingsAssessor:
     @staticmethod
     def compare_similarity(embedding1, embedding2) -> float:
+        """compare similarity between two embeddings"""
         return cosine_similarity(embedding1, embedding2)
 
 
 def cosine_similarity(vec1: list[float], vec2: list[float]):
+    """calculate cosine similarity between two vectors"""
     v1 = np.array(vec1)
     v2 = np.array(vec2)
 
